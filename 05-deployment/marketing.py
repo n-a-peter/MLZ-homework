3b77862b9b40c326d2384b9c86395687113a2060
n-a-peter <81085638+n-a-peter@users.noreply.github.com>
2025-10-30 22:50:10 +0000

    did 05-depolyment

---
+++ 05-deployment/marketing.py
@@ -0,0 +1,10 @@
+import requests
+
+url = "http://0.0.0.0:9696/predict"
+client = {
+    "lead_source": "organic_search",
+    "number_of_courses_viewed": 4,
+    "annual_income": 80304.0
+}
+conversion = requests.post(url, json=client).json()
+print("Probability of converting = ", conversion)
---
+++ 05-deployment/predict.py
@@ -0,0 +1,22 @@
+import pickle
+from typing import Dict, Any
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI(title="lead-conversion")
+
+# load the pipeline with pickle
+with open("pipeline_v1.bin", "rb") as f_in:
+    pipeline = pickle.load(f_in)
+
+@app.post("/predict")
+def predict_lead(customer: Dict[str, Any]):
+    lead_score = pipeline.predict_proba(customer)[0, 1]
+    lead_score = float(lead_score)
+    return {
+        "conversion_probability": lead_score,
+        "converted" : bool(lead_score >= 0.5)
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9696)
