ca9f7a0fab381aba1cfea7b1df1813d4701d9b57
n-a-peter <81085638+n-a-peter@users.noreply.github.com>
2025-12-14 02:05:41 +0000

    redo of 09-serverless up to Q5

---
+++ 09-serverless/test.py
@@ -0,0 +1,8 @@
+import requests
+
+url = "http://localhost:8080/2015-03-31/functions/function/invocations"
+
+hair_request = {"url": 'https://habrastorage.org/webt/yf/_d/ok/yf_dokzqy3vcritme8ggnzqlvwa.jpeg'}
+
+result = requests.post(url, json=hair_request).json()
+print(result)
